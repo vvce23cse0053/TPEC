c596a0c75ed3c9c35baf89349ae6826c29e01a36
vvce23cse0053 <vvce23cse0053@vvce.ac.in>
2025-09-26 10:15:37 +0530

    Update Mod 2.1 .java

--- TPEC-4th/Mod 2.1 .java
+++ TPEC-4th/Mod 2.1 .java
@@ -2,10 +2,12 @@
 class Checker implements Comparator<Player> {
     
     @Override
-    public int compare(Player player1, Player player2) {
+    public int compare(Player player1, Player player2)
+    {
         // First, compare scores in descending order
-        if (player1.score != player2.score) {
-            return player2.score - player1.score;
+        if (player1.score != player2.score)
+        {
+            ;
         }
         // If scores are the same, compare names in ascending order
         return player1.name.compareTo(player2.name);
